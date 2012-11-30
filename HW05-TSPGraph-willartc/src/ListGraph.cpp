6401efad3642efbf799bec34dce05363d5bd5daa
Thomas C. Willard <carywillard@yahoo.com>
2012-11-29 23:01:17 -0500

    Both List and Matrix pass all of the tests

--- HW05-TSPGraph-willartc/src/ListGraph.cpp
+++ HW05-TSPGraph-willartc/src/ListGraph.cpp
@@ -2,6 +2,7 @@
 
 ListGraph::ListGraph(int numNodes){
 	edgeList.resize(numNodes);
+	num_edges = 0;
 }
 ListGraph::~ListGraph(){
 }
@@ -10,28 +11,29 @@ ListGraph::~ListGraph(){
 // addEdge updates if the node exists, adds otherwise
 void ListGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
 	EList::const_iterator it;
-	bool insertDuplicate = false;
 	for(it = edgeList[u].begin(); it != edgeList[u].end(); it++){
 		NWPair tPair = (*it);  // The star is NOT the dereference operator, the * has been overloaded
-		if(tPair.first == v){
+			if(tPair.first == v){  // If there is an edge between u and v, we'll find it with this if
 			tPair.second = weight;
-			insertDuplicate = true;
-		}
-	}
-	// Insert complimentary edge name because the graph is undirected
-	if(insertDuplicate){
-		for(it = edgeList[v].begin(); it != edgeList[v].end(); it++){
-			NWPair tPair = (*it);  // The star is NOT the derefereence operator, the * has been overloaded
-			if(tPair.first == u){
-				tPair.second = weight;
-				num_edges++;
-				return;
+			//  Since we found the first name of the edge, we know it was inserted
+			//  Therefore, we know that the opposite name should also exist
+			//  Once we have updated both names with the new weight, we're done
+			//  So we can return without having to finish either for loop
+			//  So this isn't really a nested for loop, each loop should only run once
+			for(it = edgeList[v].begin(); it != edgeList[v].end(); it++){
+				tPair = (*it);
+				if(tPair.first == u){
+					tPair.second = weight;
+					return;
+				}
 			}
 		}
 	}
+	// If the first for loop doesn't find the edge, insert both of its names
 	edgeList[u].push_back(std::make_pair(v, weight));
-	edgeList[u].push_back(std::make_pair(v, weight));
-	num_edges++;
+
+	edgeList[v].push_back(std::make_pair(u, weight));
+	num_edges++;  //  One edge, two names: (u,v) and (v,u)
 }
 
   // Inspectors: Can read, not write
