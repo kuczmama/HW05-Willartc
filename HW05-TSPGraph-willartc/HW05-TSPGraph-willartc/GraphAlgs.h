3e33c75848ca7f4a2dc7262499ff64c8d56d534d
Thomas C. Willard <carywillard@yahoo.com>
2012-12-03 05:57:06 -0500

    Added Comments

--- HW05-TSPGraph-willartc/HW05-TSPGraph-willartc/GraphAlgs.h
+++ HW05-TSPGraph-willartc/HW05-TSPGraph-willartc/GraphAlgs.h
@@ -1,29 +1,30 @@
 // GraphAlgs.h
+
 #include "Graph.h"
 #include <vector>
 
 #pragma once
 
-static Graph* G;
-static double bestTourWeight;
-static int tourLen;
-static NodeID* bestTour;  // An array of NodeIDs that will contain the order for the shortest tour
+static Graph* G;				// The graph
+static double bestTourWeight;	// This is the min we check against
+static int tourLen;				// The number of nodes
+static NodeID* bestTour;		// An array of NodeIDs that will contain the order for the shortest tour
 
-	// Not a constructor, but similar to one.  Called by TSP!
+	// Not a constructor, but similar to one.  Called by TSP
 NodeID* tspSetup(Graph* graph);
 
-void firstTour();
-
+	// Arranges currentTour into all of the necessary permutations
+	// Calls checkTour for each permutation
 void findBestTour(int cur, double sum, NodeID* currentTour);
 
-	// Returns the sum of all of the weights in a tour.
+	// Sums all of the weights in a tour.
 	// Checks with bestTourLen after the addition of every weight
 	// Updates bestTourLen and bestTour[] if a better tour is found
-	// Returns false if not the best tour
 void checkTour(NodeID* currentTour);
 
-	// Helper method, swaps two ints.  Useful for exchanging NodeIDs in TSP
+	// Used for exchanging NodeIDs in findBestTour()
 void swapCurrentTour(int p, int q, NodeID* currentTour);
+
 
 /*
  * Solves the Traveling Salesperson Problem: finding the shortest cycle through a graph that 
